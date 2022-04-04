1f3b9c9f882429ed0640302f59cbb0d5ab7ae2ed
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-04-04 16:49:00 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,12 @@
+import sys
+
 def main():
-    print("Hello World!")
+    print("Hello World! From Python: " + str(sys.version_info))
+    if sys.version_info.+ (3,6) and sys.version_info < (3,7):
+        #let's make this script faily for Python 3.6
+        raise Exception('Python version 3.6x is unsupported!')
+        
+        
+        
 if __name__ == "__main__":
     main()
-    
